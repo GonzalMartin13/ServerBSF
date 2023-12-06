f984d69b04d4ef66e70fe8bbc5a59b0007011912
gonzalo <gonzalo.m.martin@gmail.com>
2023-12-06 00:18:20 -0300

    s

--- Server/src/handlers/handlerAllEnvios.js
+++ Server/src/handlers/handlerAllEnvios.js
@@ -6,7 +6,7 @@ const handlerAllEnvios = async (req, res) => {
     try {
         const response = await getAllPackage();
         !response ? res.status(404).json({ error: 'No se encontraron Envios' })
-            : res.status(200).json("Holis");
+            : res.status(200).json(response);
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
