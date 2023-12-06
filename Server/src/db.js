b06ecf464ce6094a31ea1add6b0a28deedef8c5e
gonzalo <gonzalo.m.martin@gmail.com>
2023-12-06 00:04:41 -0300

    deplay

--- Server/src/db.js
+++ Server/src/db.js
@@ -4,9 +4,16 @@ const pg = require("pg");
 const path = require("path");
 const fs = require("fs");
 
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DEPLOY_URL } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/BFS`,
+const sequelize = new Sequelize(DEPLOY_URL,	
+	{
+		logging: false,
+		native: false,
+		dialectModule: pg,
+	}
+);
+/* const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/BFS`,
 	
 	{
 		logging: false,
@@ -14,7 +21,7 @@ const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}
 		dialectModule: pg,
 	}
 );
-
+ */
 const basename = path.basename(__filename);
 
 // Lectura y carga dinámica de modelos desde la carpeta 'models'
